1a6caaac157182b8fb7f3112d179eb00bc37940a
Erik Bartoš <erik.bartos@gmail.com>
2025-11-25 11:42:47 +0100

    package completion

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,19 +1,24 @@
 """ Tests
-    Run in home directory 'bvpasion':
+    Run in home directory 'datalock':
     > python -m pytest
 
 """
+import pytest
 # import numpy as np
 from numpy.testing import assert_allclose
 
 from datalock.experimentalData import ExperimentalData
 
 
+@pytest.fixture
+def exd():
+    """Creates a fresh instance of ExperimentalData before each test."""
+    return ExperimentalData()
+
+def test_empty_dataset():
+    exd = ExperimentalData()
+    assert exd.count == 0
+    assert exd.sets == []
+
 def test_always_passes():
     assert True
-
-def test_uppercase():
-    assert "loud noises".upper() == "LOUD NOISES"
-
-def test_reversed():
-    assert list(reversed([1, 2, 3, 4])) == [4, 3, 2, 1]
